daff57068fe30dcbb2ba2c45b0ea070dd8cf171d
yowger <106136772+yowger@users.noreply.github.com>
2023-05-05 03:37:21 +0800

    Update apiSlice.js

--- src/app/api/apiSlice.js
+++ src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const apiSlice = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:4500",
+        baseUrl: "https://portfolio-server-mjm2.onrender.com",
     }),
     endpoints: () => ({}),
 })
